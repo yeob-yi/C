5b8c997f8d695ec6833dcfc6f1a356796226302d
yeob-yi <happyk100@naver.com>
2021-10-04 21:33:36 +0900

    C_hw6

--- C/hw6_22100014_Kangshinyeob.c
+++ C/hw6_22100014_Kangshinyeob.c
@@ -4,27 +4,25 @@
 int main(void){
     int n1, n2;
     int i;
-    int no=0;
 
     printf("두 개의 정수(n1, n2)를 입력하세요. ");
     scanf("%d %d", &n1, &n2);
-
-    if(n1>n2){
-        i=n2;
-    }else{
-        i=n1;
-    }
     
-    while(i>0){
-        if(n1%i==0 && n2%i==0){
-            no=1;
-            printf("최대 공약수는 %d입니다.", i);
-            break;
+    if(n1<1 || n2<1){
+        printf("음수를 입력하였습니다.");
+    }else{
+        if(n1>n2){
+            i=n2;
+        }else{
+            i=n1;
         }
-        i--;
-    }
-    if(no==0){
-        printf("공약수가 존재하지 않습니다.");
+        while(i>0){
+            if(n1%i==0 && n2%i==0){
+                printf("최대 공약수는 %d입니다.", i);
+                break;
+            }
+            i--;
+        }
     }
 
     return 0;
