9cba848700438e5a32c0c9cbcd28d19df31f1936
yeob-yi <happyk100@naver.com>
2021-10-04 20:32:51 +0900

    C_hw4

---
+++ C/hw4_22100014_Kangshinyeob.c
@@ -0,0 +1,20 @@
+// 한 개의 정수 값(년도)을 입력받아 윤년인지 여부를 판단하는 프로그램
+#include <stdio.h>
+
+int main(void){
+    int year;
+
+    printf("연도를 입력하세요. ");
+    scanf("%d", &year);
+
+    if(year%400==0){
+        printf("윤년입니다.");
+    }else if(year%100==0){
+        printf("윤년이 아닙니다.");
+    }else if(year%4==0){
+        printf("윤년입니다.");
+    }else{
+        printf("윤년이 아닙니다.");
+    }
+    return 0;
+}
