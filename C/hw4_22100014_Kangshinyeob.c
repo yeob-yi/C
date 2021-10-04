f747d81e0493f5ea2ed8d142548deb02ad1dd66f
yeob-yi <happyk100@naver.com>
2021-10-04 21:02:00 +0900

    C_hw4

--- C/hw4_22100014_Kangshinyeob.c
+++ C/hw4_22100014_Kangshinyeob.c
@@ -7,14 +7,13 @@ int main(void){
     printf("연도를 입력하세요. ");
     scanf("%d", &year);
 
-    if(year%400==0){
-        printf("윤년입니다.");
-    }else if(year%100==0){
+    if(year%100==0 && year%400!=0){
         printf("윤년이 아닙니다.");
     }else if(year%4==0){
         printf("윤년입니다.");
     }else{
         printf("윤년이 아닙니다.");
     }
+    
     return 0;
 }
