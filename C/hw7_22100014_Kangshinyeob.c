4d737741cc7c833bdb3e65f212fccdc1bb765a0d
yeob-yi <happyk100@naver.com>
2021-10-07 22:44:44 +0900

    C_hw7

--- C/hw7_22100014_Kangshinyeob.c
+++ C/hw7_22100014_Kangshinyeob.c
@@ -2,22 +2,30 @@
 #include <stdio.h>
 
 int main(void){
-    int n; 
-    int sum=0;
+    int n;
+    int y=0;
+    int sum;
 
-    printf("정수 입력: ");
+    printf("2 이상의 정수 입력: ");
     scanf("%d", &n);
-
-    for(int i=1; i<n; i++){
-        if(n%i==0){
-            sum+=i;
-        }        
+    
+    for(int i=1; i<=n; i++){
+        sum=0;
+        for(int j=1; j<i; j++){
+            if(i%j==0){
+                sum+=j;
+            }
+        }
+        if(i==sum){
+            printf("%d ", i);
+            y=1;
+        }
     }
 
-    if(sum==n){
-        printf("완전수입니다.");
-    }else{
-        printf("완전수가 아닙니다.");
-    }
+    if(y==1)
+        printf("은(는) 완전수 입니다.");
+    else
+        printf("1부터 %d 범위의 정수 중에는 완전수가 없습니다.", n);
+        
     return 0;
 }
